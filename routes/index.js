186e3ae3b74a74967734c09052c9f047f3bcff10
Megha Patel <46495166+PatelMegha1@users.noreply.github.com>
2019-10-10 14:59:23 -0400

    js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Megha Patel' });
 });
+router.get('/', function(req, res, next) {
+  res.render('/home.js');
+});
 
 module.exports = router;
